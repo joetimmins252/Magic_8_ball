dce3a0be8978f9b5447fa0e6685a40059af1fea1
Joseph Timmins <josephtimmins@Josephs-MacBook-Air.local>
2017-04-25 17:38:29 -0400

    magic 8 ball code

---
+++ index.py
@@ -0,0 +1,24 @@
+import sys
+import random
+import time
+
+wisdom = [
+'There is no doubt in my wisdom',
+'In your dreams',
+'get reaallllll haha',
+'most likely yes',
+'possibly yes',
+'maybe if you read a book',
+'Ask later',
+'are you crazy? yes',
+'Hazy future',
+'I regret to inform you not gonna happen',
+'You won a million dollars!!!!'
+]
+while True:
+ 	input("Ask the ultimate wisdom of the mighty 8-ball a qestion:")
+ 	for i in range(0,1):
+ 		print('feeling the answers within me...')
+ 		time.sleep(1
+ 			)
+ 		print(random.choice(wisdom))
